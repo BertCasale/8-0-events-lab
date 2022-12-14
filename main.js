c706c995c6b11d2780388aa47a4240f15efe057a
BertCasale <bthebest55@gmail.com>
2022-12-13 20:26:01 -0500

    added reset and fill buttons

--- main.js
+++ main.js
@@ -31,30 +31,48 @@ for (const cell of cells) {
   //add click event to the cells
   cell.addEventListener("click", () => {
     //set the cells background color to the current colors backgrund color
-    cell.style.backgroundColor = currentColor.style.backgroundColor ;
+    cell.style.backgroundColor = currentColor.style.backgroundColor;
   })
 }
 
-//reset button, just for fun
 
-// //create a div for the buttons
-// const buttonDiv = document.createElement("div")
-// //center it on screen
-// buttonDiv.setAttribute("align", "center");
-// //put div after the current color element
-// currentColor.after(buttonDiv);
+//buttons, just for fun
 
-// //create button
-// const reset = document.createElement("button");
-// reset.textContent = "Reset Canvas";
-// //append button to div
-// buttonDiv.append(reset);
+//create a div for the buttons
+const buttonDiv = document.createElement("div")
+//center it on screen
+buttonDiv.setAttribute("align", "center");
+//put div after the current color element
+currentColor.after(buttonDiv);
 
-// //add click event to reset button
-// reset.addEventListener("click", () => {
-//   //loop through cells
-//   for (const cell of cells) {
-//     //remove the background color
-//     cell.backgroundColor.remove();
-//   }
-// })
+//create reset button
+const reset = document.createElement("button");
+//enter button text
+reset.textContent = "Reset Canvas";
+//append button to div
+buttonDiv.append(reset);
+
+//add click event to reset button
+reset.addEventListener("click", () => {
+  //loop through cells
+  for (const cell of cells) {
+    //remove the background color
+    cell.style.backgroundColor = "";
+  }
+});
+
+//create fill button
+const fill = document.createElement("button");
+//enter button text
+fill.textContent = "Fill Canvas";
+//append button to div
+buttonDiv.append(fill);
+
+//add click event to fill button
+fill.addEventListener("click", () => {
+  //loop through cells
+  for (const cell of cells) {
+    //set the background color to the current color
+    cell.style.backgroundColor = currentColor.style.backgroundColor;
+  }
+});
